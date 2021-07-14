eb47e85548b8026f765594d85ffbbb28c26a5608
Rob Stinson <r.stinson11@gmail.com>
2021-07-14 11:36:31 +1000

    Add plugin php header comment

---
+++ progress-indicator.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       Progress Indicator Block
+ * Plugin URI:        https://robstinson.co/progress-indicator
+ * Description:       A block that lets you easily display a progress indicator on your WordPress posts or pages.
+ * Version:           0.1.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Rob Stinson
+ * Author URI:        https://robstinson.co
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       progress-indicator
+ * Domain Path:       /languages
+ */
