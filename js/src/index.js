463b08be1fcb468e10a309dc5bcad361f49ea0ff
Rob Stinson <r.stinson11@gmail.com>
2021-08-06 10:35:42 +1000

    Clean up index.js and make strings translatable

--- js/src/index.js
+++ js/src/index.js
@@ -1,13 +1,34 @@
 /**
  * WordPress dependencies
  */
-
 import { registerBlockType } from '@wordpress/blocks';
-import * as block from './block.json';
+import { __ } from '@wordpress/i18n';
 
-registerBlockType( block.name,
+/**
+ * Internal dependencies
+ */
+import { 	
+	apiVersion,
+	name,
+	category,
+	icon,
+	attributes
+} from './block.json';
+import Edit from './edit';
+
+registerBlockType( name,
 	{
-		...block,
-		edit: () => <p>This is the block</p>,
+		apiVersion,
+		title: __( 'Progress Indicator', 'progress-indicator' ),
+		description: __( 'A block that lets you easily display a progress indicator on your WordPress posts or pages.', 'progress-indicator' ),
+		keywords: [
+			__( 'progress', 'progress-indicator' ),
+			__( 'steps', 'progress-indicator' ),
+		],
+		category,
+		icon,
+		attributes,
+		edit: Edit,
+		save: () => null,
 	}
 );
