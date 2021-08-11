ab2cc292f9ef10f746d20e229cf401191f58b97a
Rob Stinson <r.stinson11@gmail.com>
2021-08-11 12:46:27 +1000

    Add template markup

--- js/src/edit.js
+++ js/src/edit.js
@@ -1,12 +1,55 @@
 /**
  * WordPress dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { ColorPalette, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+import ServerSideRender from '@wordpress/server-side-render';
 
-const Edit = () => {
+/**
+ * Internal dependencies
+ */
+import { name } from './block.json';
+
+const Edit = ( {
+	attributes,
+	setAttributes,
+} ) => {
 	const blockProps = useBlockProps();
+	const { colors } = useSelect( ( select ) => select( 'core/block-editor' ).getSettings() );
+	return <div { ...blockProps }>
 
-	return <div { ...blockProps }></div>;
+		{ /* For next time, reimplement PHP template here in JS */ }
+		<ServerSideRender
+			attributes={ attributes }
+			block={ name }
+		/>
+		<InspectorControls>
+			<TextControl
+				label={ __( 'Number of Steps', 'progress-indicator' ) }
+				type="number"
+				value={ attributes.numberOfSteps }
+				onChange={ ( newValue ) => {
+					setAttributes( { numberOfSteps: Number( newValue ) } );
+				} }
+			/>
+			<TextControl
+				label={ __( 'Current Step', 'progress-indicator' ) }
+				type="number"
+				value={ attributes.currentStep }
+				onChange={ ( newValue ) => {
+					setAttributes( { currentStep: Number( newValue ) } );
+				} }
+			/>
+			<ColorPalette
+				label={ __( 'Color', 'progress-indicator' ) }
+				colors={ colors }
+				value={ attributes.color }
+				onChange={ ( newValue ) => setAttributes( { color: newValue } ) }
+			/>
+		</InspectorControls>
+	</div>;
 };
 
 export default Edit;
--- progress-indicator.php
+++ progress-indicator.php
@@ -17,5 +17,51 @@
 add_action( 'init', 'progress_indicator_register_block' );
 
 function progress_indicator_register_block() {
-	register_block_type_from_metadata( __DIR__ . '/js/src' );
+	register_block_type_from_metadata( __DIR__ . '/js/src', [
+		'render_callback' => function ( $attributes ) { 
+			$steps = $attributes['numberOfSteps'];
+			$current_step = $attributes['currentStep'];
+			ob_start();
+			?>
+			<div class="gcb-progress-indicator">
+
+			<!-- Step Lines  -->
+			<div class="gcb-progress-indicator__lines">
+				<?php 
+				for ( $i = 1; $i < $steps; $i++ ):
+					$line_classes = $i < $current_step ? 'gcb-progress-indicator__line gcb-progress-indicator__complete-line' : 'gcb-progress-indicator__line';
+					?>
+					<div class="<?php echo esc_attr( $line_classes ); ?>"></div>
+				<?php endfor; ?>
+			</div>
+
+			<!-- Step Numbers -->
+			<?php for ($i = 1; $i <= $steps; $i++):
+				$step_classes = 'gcb-progress-indicator__step';
+				if ($i === $current_step) {
+					$step_classes .= 
+						' gcb-progress-indicator__current-step';
+				} elseif ($i < $current_step) {
+					$step_classes .= 
+						' gcb-progress-indicator__complete-step';
+				}
+				?>
+
+				<div class="<?php echo esc_attr($step_classes); ?>">
+					<?php if ($i < $current_step) {
+						echo '<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 stroke-current text-green-50" viewBox="0 0 20 20" fill="currentColor">
+							<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+						</svg>';
+					} else {
+						echo esc_html($i);
+					} ?>
+				</div>
+
+			<?php endfor; ?>
+			</div>
+
+			<?php
+			return ob_get_clean();
+		}
+	] );
 }
