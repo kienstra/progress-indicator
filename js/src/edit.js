5ac368292ce13a2569f00066ff4fb0acdbec91f7
Rob Stinson <r.stinson11@gmail.com>
2021-08-18 12:53:36 +1000

    Removed label from color pallette component.

--- js/src/edit.js
+++ js/src/edit.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { ColorPalette, TextControl } from '@wordpress/components';
+import { ColorPalette, TextControl, PanelBody, ColorIndicator } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import ProgressIndicator from './progress-indicator';
@@ -15,30 +15,31 @@ const Edit = ( {
 	return <div { ...useBlockProps() }>
 		<ProgressIndicator attributes={ attributes } />
 		<InspectorControls>
-			<TextControl
-				label={ __( 'Number of Steps', 'progress-indicator' ) }
-				type="number"
-				value={ attributes.numberOfSteps }
-				onChange={ ( newValue ) => {
-					setAttributes( { numberOfSteps: Number( newValue ) } );
-				} }
-				max={ 10 }
-			/>
-			<TextControl
-				label={ __( 'Current Step', 'progress-indicator' ) }
-				type="number"
-				value={ attributes.currentStep }
-				onChange={ ( newValue ) => {
-					setAttributes( { currentStep: Number( newValue ) } );
-				} }
-				max={ attributes.numberOfSteps }
-			/>
-			<ColorPalette
-				label={ __( 'Color', 'progress-indicator' ) }
-				colors={ colors }
-				value={ attributes.color }
-				onChange={ ( newValue ) => setAttributes( { color: newValue } ) }
-			/>
+			<PanelBody>
+				<TextControl
+					label={ __( 'Number of Steps', 'progress-indicator' ) }
+					type="number"
+					value={ attributes.numberOfSteps }
+					onChange={ ( newValue ) => {
+						setAttributes( { numberOfSteps: Number( newValue ) } );
+					} }
+					max={ 10 }
+				/>
+				<TextControl
+					label={ __( 'Current Step', 'progress-indicator' ) }
+					type="number"
+					value={ attributes.currentStep }
+					onChange={ ( newValue ) => {
+						setAttributes( { currentStep: Number( newValue ) } );
+					} }
+					max={ attributes.numberOfSteps }
+				/>
+				<ColorPalette
+					colors={ colors }
+					value={ attributes.color }
+					onChange={ ( newValue ) => setAttributes( { color: newValue } ) }
+				/>
+			</PanelBody>
 		</InspectorControls>
 	</div>;
 };
