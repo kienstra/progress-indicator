29ba1869d494255c16b0ac49c56da42ff9185a09
Ryan Kienstra <kienstraryan@gmail.com>
2022-01-17 17:10:03 -0600

    Change step to stepNumber, add jsx-ally plugin

--- js/src/progress-indicator.js
+++ js/src/progress-indicator.js
@@ -14,31 +14,34 @@ export default ( {
 			{ [ ...Array( attributes.numberOfSteps - 1 ) ].map( ( value, index ) =>
 				<div
 					key={ index }
-					className={ index + 1 < attributes.currentStep
+					className={ attributes.currentStep > index + 1
 						? 'gcb-progress-indicator__line gcb-progress-indicator__complete-line'
 						: 'gcb-progress-indicator__line'
-					} />
+					}
+				/>
 			) }
 		</div>
 		{ /* Step Circles */ }
 		{ [ ...Array( attributes.numberOfSteps ) ].map( ( value, index ) => {
+			const stepNumber = index + 1;
 			let stepClasses = 'gcb-progress-indicator__step';
-			const step = index + 1;
-			if ( step === attributes.currentStep ) {
+
+			if ( stepNumber === attributes.currentStep ) {
 				stepClasses += ' gcb-progress-indicator__current-step';
-			} else if ( step < attributes.currentStep ) {
+			} else if ( stepNumber < attributes.currentStep ) {
 				stepClasses += ' gcb-progress-indicator__complete-step';
 			}
+
 			return <div
 				key={ index }
 				className={ stepClasses }
 				style={ { backgroundColor: attributes.color } }
 			>
-				{ step < attributes.currentStep
-					? <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 stroke-current text-green-50" viewBox="0 0 20 20" fill="currentColor">
+				{ attributes.currentStep > stepNumber
+					? <svg role="img" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 						<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
 					</svg>
-					: step
+					: stepNumber
 				}
 			</div>;
 		} ) }
