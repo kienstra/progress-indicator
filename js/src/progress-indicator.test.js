2a0fc7ecc39c8d0f5914099a0838620b25ae1401
Ryan Kienstra <kienstraryan@gmail.com>
2022-01-25 19:19:26 -0600

    Run the Jest test in CircleCI

---
+++ js/src/progress-indicator.test.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import ProgressIndicator from './progress-indicator';
+
+test( 'ProgressIndicator with current step of 2/5', () => {
+	render(
+		<ProgressIndicator
+			attributes={ {
+				numberOfSteps: 5,
+				currentStep: 2,
+			} }
+		/>
+	);
+
+	it.each( [ 2, 3, 4, 5 ], ( number ) => {
+		expect( screen.getByText( number ) ).toBeInTheDocument();
+	} );
+
+	expect( screen.getAllByRole( 'img' ).length ).toEqual( 1 );
+} );
+
+test( 'ProgressIndicator with current step of 5/5', () => {
+	render(
+		<ProgressIndicator
+			attributes={ {
+				numberOfSteps: 5,
+				currentStep: 5,
+			} }
+		/>
+	);
+
+	expect( screen.getByText( 5 ) ).toBeInTheDocument();
+	expect( screen.getAllByRole( 'img' ).length ).toEqual( 4 );
+} );
