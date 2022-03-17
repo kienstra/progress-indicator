34bd1735fce16a8ce4893573f5a953d185026973
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-16 22:44:47 -0600

    Update index.final.js for the latest changes to main

--- js/src/index.exercise.js
+++ js/src/index.exercise.js
@@ -7,18 +7,19 @@ import { registerBlockType } from '@wordpress/blocks';
  * Internal dependencies
  */
 import block from '../../block.json';
-// 🚧 Import the Edit component.
-// Import the Save component.
+// 🚧 Import the edit component.
+// Import the save component.
 
 /**
- * @typedef {Object} Attributes The block attributes.
- * @property {string} color         The color of the indicators.
- * @property {number} currentStep   The step that the indicator is on.
- * @property {number} numberOfSteps The total number of steps in the indicator.
+ * @typedef {{
+ *  color: typeof block.attributes.color.default,
+ *  currentStep: typeof block.attributes.currentStep.default,
+ *  numberOfSteps: typeof block.attributes.numberOfSteps.default
+ * }} Attributes
  */
 
-// @ts-ignore The declaration file is probably wrong.
+// @ts-ignore The declaration file is wrong.
 registerBlockType( block.name, {
-	// 🚧 For property 'edit', pass the Edit component imported above.
-	// For property 'save', pass the Save component imported above.
+	// 🚧 For property 'edit', pass the edit component imported above.
+	// For property 'save', pass the save component imported above.
 } );
--- js/src/index.final.js
+++ js/src/index.final.js
@@ -2,44 +2,24 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import block from '../../block.json';
-import Edit from './edit';
-import Save from './save';
-
-const { apiVersion, attributes, category, icon, keywords, name } = block;
+import edit from './edit';
+import save from './save';
 
 /**
- * @typedef {Object} Attributes The block attributes.
- * @property {string} color         The color of the indicators.
- * @property {number} currentStep   The step that the indicator is on.
- * @property {number} numberOfSteps The total number of steps in the indicator.
+ * @typedef {{
+ *  color: typeof block.attributes.color.default,
+ *  currentStep: typeof block.attributes.currentStep.default,
+ *  numberOfSteps: typeof block.attributes.numberOfSteps.default
+ * }} Attributes
  */
 
-// @ts-ignore The declaration file is probably wrong.
-registerBlockType( name, {
-	apiVersion,
-	title: __( 'Progress Indicator', 'progress-indicator' ),
-	description: __(
-		'A block that lets you easily display a progress indicator on your WordPress posts or pages.',
-		'progress-indicator',
-	),
-	attributes,
-	category,
-	icon,
-	keywords,
-	example: {
-		/** @type {Attributes} */
-		attributes: {
-			color: '#10b981',
-			currentStep: 2,
-			numberOfSteps: 5,
-		},
-	},
-	edit: Edit,
-	save: Save,
+// @ts-ignore The declaration file is wrong.
+registerBlockType( block.name, {
+	edit,
+	save,
 } );
